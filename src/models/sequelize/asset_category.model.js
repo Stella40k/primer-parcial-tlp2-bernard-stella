d094af09b61e244285774b7a4634fbc6d6074c0c
Stella40k <bernardstella0704@gmail.com>
2025-09-29 10:50:26 -0300

    fix: asset_category.model, relaciones y creacion de tabla intermedia

--- src/models/sequelize/asset_category.model.js
+++ src/models/sequelize/asset_category.model.js
@@ -1,4 +1,6 @@
 import { DataTypes } from "sequelize";
+import { AssetModel } from "./asset.model.js";
+import { CategoryModel } from "../mongoose/category.model.js";
 
 export const AssetCategoryModel = sequelize.define("AssetCategory", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
@@ -8,3 +10,30 @@ export const AssetCategoryModel = sequelize.define("AssetCategory", {
 // * N:M Asset ↔ Category through AssetCategory
 // * 'categories' (Asset) y 'assets' (Category)
 // ! FALTA COMPLETAR ACA
+
+//relaciones con tabla intermedia
+AssetModel.belongsToMany(CategoryModel, {
+  through: CategoryModel,
+  foreignKey: "category_id",
+  as: "category",
+  onDelete: CASCADE,
+});
+CategoryModel.belongsToMany(AssetModel, {
+  through: AssetModel,
+  foreignKey: "asset_id",
+  as: "asset",
+  onDelete: CASCADE,
+});
+
+//relaciones para la tabla intermedia
+AssetCategoryModel.belongsTo(CategoryModel,{
+  foreignKey: "category",
+  as: "asset",
+  onDelete: CASCADE
+});
+AssetCategoryModel.belongsTo(AssetModel,{
+  foreignKey: "asset_id",
+  as: "asset",
+  onDelete: CASCADE
+});
+ 
